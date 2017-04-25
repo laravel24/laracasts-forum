bffa34164e0078169dc7cb3e5c3683959dd836c4
Ciprian <ciprian88m@gmail.com>
2017-04-25 06:20:49 +0300

    Let's make some testing helpers

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -20,9 +20,9 @@ class ParticipateInForumTest extends TestCase
   /** @test */
   public function an_authenticated_user_may_participate_in_forum_threads()
   {
-    $this->be($user = factory('App\User')->create());
-    $thread = factory('App\Thread')->create();
-    $reply = factory('App\Reply')->make();
+    $this->signIn();
+    $thread = create('App\Thread');
+    $reply = make('App\Reply');
     $this->post($thread->path().'/replies', $reply->toArray());
     $this->get($thread->path())
       ->assertSee($reply->body);
