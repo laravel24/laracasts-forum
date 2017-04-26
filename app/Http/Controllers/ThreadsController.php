ef577855552c57df370af8c442733ab5d0d12538
Ciprian <ciprian88m@gmail.com>
2017-04-26 06:08:35 +0300

    A thread should be assigned a channel

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -45,7 +45,8 @@ class ThreadsController extends Controller
       $thread = Thread::create([
         'user_id' => auth()->id(),
         'title' => request('title'),
-        'body' => request('body')
+        'body' => request('body'),
+        'channel_id' => request('channel_id')
       ]);
 
       return redirect($thread->path());
@@ -57,7 +58,7 @@ class ThreadsController extends Controller
      * @param  \App\Thread  $thread
      * @return \Illuminate\Http\Response
      */
-    public function show(Thread $thread)
+    public function show($channelId, Thread $thread)
     {
       return view('threads.show', compact('thread'));
     }
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -19,9 +19,10 @@ class ThreadTest extends TestCase
   }
 
   /** @test */
-  public function a_thread_has_replies()
+  public function a_thread_can_make_a_string_path()
   {
-    $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
+    $thread = create('App\Thread');
+    $this->assertEquals("/threads/{$thread->channel->slug}/{$thread->id}", $thread->path());
   }
 
   /** @test */
@@ -39,6 +40,19 @@ class ThreadTest extends TestCase
     ]);
 
     $this->assertCount(1, $this->thread->replies);
+  }
+
+  /** @test */
+  public function a_thread_has_replies()
+  {
+    $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
+  }
+
+  /** @test */
+  public function a_thread_belongs_to_a_channel()
+  {
+    $thread = create('App\Thread');
+    $this->assertInstanceOf('App\Channel', $thread->channel);
   }
 
 }
