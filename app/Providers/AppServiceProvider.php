9b2af686cf7b49b72499ba5dab98056246f600a9
Ciprian <ciprian88m@gmail.com>
2017-04-30 08:55:08 +0300

    From 56 queries down to 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,13 +10,19 @@ class AppServiceProvider extends ServiceProvider
   public function boot()
   {
     \View::composer('*', function($view) {
-      $view->with('channels', \App\Channel::all());
+      $channels = \Cache::rememberForever('channels', function() {
+        return \App\Channel::all();
+      });
+
+      $view->with('channels', $channels);
     });
   }
 
   public function register()
   {
-
+    if($this->app->isLocal()) {
+      $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+    }
   }
 
 }
