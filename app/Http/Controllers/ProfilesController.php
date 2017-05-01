dcbec6dde3ce5032a780e10a6f468cbe1fd20163
Ciprian <ciprian88m@gmail.com>
2017-05-01 16:11:30 +0300

    Extracting controller queries to the model

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\User;
+use App\{User, Activity};
 
 class ProfilesController extends Controller
 {
@@ -13,15 +13,8 @@ class ProfilesController extends Controller
   {
     return view('profiles.show', [
       'profileUser' => $user,
-      'activities' => \App\Activity::feed($user)
+      'activities' => Activity::feed($user)
     ]);
-  }
-
-  protected function getActivity(User $user)
-  {
-    return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function($activity) {
-      return $activity->created_at->format('Y-m-d');
-    });
   }
 
 }
--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -4,6 +4,9 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Carbon\Carbon;
+
+use App\Activity;
 
 class ActivityTest extends TestCase
 {
@@ -23,18 +26,37 @@ class ActivityTest extends TestCase
       'subject_type' => 'App\Thread'
     ]);
 
-    $activity = \App\Activity::first();
+    $activity = Activity::first();
 
     $this->assertEquals($activity->subject->id, $thread->id);
   }
 
   /** @test */
-  function it_records_activity_when_a_reply_is_created()
+  public function it_records_activity_when_a_reply_is_created()
   {
     $this->signIn();
     $reply = create('App\Reply');
 
-    $this->assertEquals(2, \App\Activity::count());
+    $this->assertEquals(2, Activity::count());
+  }
+
+  /** @test */
+  public function it_fetches_an_activity_feed_for_any_user()
+  {
+    $this->signIn();
+    create('App\Thread', [ 'user_id' => auth()->id() ], 2);
+
+    auth()->user()->activity()->first()->update([ 'created_at' => Carbon::now()->subWeek() ]);
+
+    $feed = Activity::feed(auth()->user());
+
+    $this->assertTrue($feed->keys()->contains(
+      Carbon::now()->format('Y-m-d')
+    ));
+
+    $this->assertTrue($feed->keys()->contains(
+      Carbon::now()->subWeek()->format('Y-m-d')
+    ));
   }
 
 }
